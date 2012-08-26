505c3c6dd00d1b78913642d04f4bcf014bacef29
dryes <joswiseman@gmail.com>
2012-08-26 11:58:34 +0100

    redone using class and not forced css

--- nzbshighlightrow.user.js
+++ nzbshighlightrow.user.js
@@ -4,17 +4,19 @@
 // @description    Highlight table row on mouseover with classic theme.
 // @include        http*://www.nzbs.org/*
 // @include        http*://nzbs.org/*
-// @version        0002
+// @version        0003
 // @updateURL      https://github.com/dryes/nzbsdotorg_gm/raw/master/nzbshighlightrow.user.js
 // ==/UserScript==
 $ = unsafeWindow.$;
 
 function main() {
+    GM_addStyle('.mouseover { background-color: ' + ($('body').attr('class') == 'classic' ? '#CDF4F4' : '#7F85AB') + ' !important; }');
+
     $('tbody').delegate('tr', 'mouseover mouseleave', function (event) {
         if (event.type == 'mouseover') {
-            $(this).css('background-color', ($('body').attr('class') == 'classic' ? '#CDF4F4' : '#7F85AB'));
+            $(this).addClass('mouseover');
         } else {
-            $(this).css('background-color', '');
+            $(this).removeClass('mouseover');
         }
     });
 }
